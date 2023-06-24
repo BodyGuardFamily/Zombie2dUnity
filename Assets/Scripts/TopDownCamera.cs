c9f0509893de22ca99e2ca2c93846a6cfe440a57
Jorge Zepeda <baattts@gmail.com>
2023-06-24 14:56:55 -0700

    Added a movement script, as well as a camera to follow the player. Added some free concrete materials to use for the building. Set up a simple scene

---
+++ Assets/Scripts/TopDownCamera.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class TopDownCamera : MonoBehaviour
+{
+    // The player
+    public GameObject target;
+
+    public Vector3 offset = new Vector3(0, 0, -1);
+    // Update is called once per frame
+    // transform camera position to player
+    private void FixedUpdate()
+    {   
+        // if player exist adjust camera position
+        if (target)
+        {
+            var position = target.transform.position;
+            transform.position = new Vector3(
+                position.x + offset.x, 
+                position.y + offset.y, 
+                position.z + offset.z);
+        }
+        throw new NotImplementedException();
+    }
+}
